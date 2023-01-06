971705db157417232d6ee0a5f791db954a8034a9
Doug Arnold <doug.arnold@cgi.com>
2023-01-06 09:23:34 +0000

    Upgraded website

--- index.js
+++
@@ -1,5 +0,0 @@
-function popup() {
-    alert("Hello! I am an alert box!");
-  }
-
-document.getElementById('date').innerHTML = new Date().toDateString();
---
+++ script.js
@@ -0,0 +1,10 @@
+function quote(){
+  alert("“The best time to plant a tree was 20 years ago.\n The second best time is now.” ― Chinese Proverb")
+}
+
+
+function popup() {
+  alert("Hello! I am an alert box!");
+}
+
+document.getElementById('date').innerHTML = new Date().toDateString();
